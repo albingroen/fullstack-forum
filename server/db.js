021f5a23cc78a3e07f21c657a4348d527ba9556f
Albin Groen <albin.groen@gmail.com>
2018-09-09 20:55:26 +0200

    Added a public user so we can host this with Zeit now for demo purposes.

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_HOST = process.env.DB_HOST;
-const DB_NAME = process.env.DB_NAME;
+const DB_USER = "admin" || process.env.DB_USER;
+const DB_PASSWORD = "admin123" || process.env.DB_PASSWORD;
+const DB_HOST = "ds151012.mlab.com:51012" || process.env.DB_HOST;
+const DB_NAME = "example-forum" || process.env.DB_NAME;
 
 const mongoUrl = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
 
